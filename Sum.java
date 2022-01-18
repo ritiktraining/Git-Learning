3e0232a4bddbc6b2ac14d449f2195ea608cab71d
Ritik <contact.ritik1@gmail.com>
2022-01-18 14:10:09 +0530

    d is added

--- Sum.java
+++ Sum.java
@@ -1,9 +1,13 @@
+
+
+
 public class Sum{
 public static void main(String[] args)
 {
 int a=10;
 int b=20;
-int c=a+b;
-System.out.println("The Sum is" + c);
+int d=a+b;
+System.out.println("The Sum is" + d);
 }
+
 }
